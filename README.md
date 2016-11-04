304f6399cbf2f46308287c524a00d9a935582d22
Findley, Randy <rgfindley@gmail.com>
2016-11-04 11:21:09 -0400

    Update lambda function.

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # lambda-nginx-nodejs-proxy
 API Proxy to AWS Lambda's, using nginx, nodejs, and running on EC2.
 
-## User Data
+##
+Spin up an EC2 running Amazon Linux and use the User Data below.
+
+### User Data
 ```
 #!/bin/bash
 # Instal nginx, git and start
 sudo yum update -y
 sudo yum install -y nginx git
-sudo service nginx start
+service nginx start
 
 # Install node & npm
 sudo yum install -y --enablerepo=epel npm
@@ -15,12 +18,13 @@ sudo npm install -g forever
 
 # Pull down code, install deps, and run
 sudo git clone https://github.com/rgfindl/lambda-nginx-nodejs-proxy
-sudo cd lambda-nginx-nodejs-proxy
+cd lambda-nginx-nodejs-proxy
 sudo npm install
 sudo npm start
+cd /
 
 # Configure nginx
-sudo cp lambda-nginx-nodejs-proxy/node_backend.conf /etc/nginx/conf.d/.
-sudo cp lambda-nginx-nodejs-proxy/api.conf /etc/nginx/default.d/.
-sudo service nginx restart
+sudo cp /lambda-nginx-nodejs-proxy/node_backend.conf /etc/nginx/conf.d/.
+sudo cp /lambda-nginx-nodejs-proxy/api.conf /etc/nginx/default.d/.
+service nginx restart
 ```
--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var http = require('http');
+var URL = require('url');
+var AWS = require('aws-sdk');
+
 http.createServer(function(request, response) {
     var body = [];
     var url = request.url;
+    console.log(url);
+    var url_parsed = URL.parse(url, true);
+    console.log(JSON.stringify(url_parsed, null, 3));
     request.on('error', function(err) {
         console.error(err);
     }).on('data', function(chunk) {
@@ -13,8 +19,41 @@ http.createServer(function(request, response) {
             console.error(err);
         });
 
-        response.statusCode = 200;
-        response.write('Hello World 2!');
-        response.end();
+        var lambda = new AWS.Lambda();
+        console.log(request.method);
+        if (request.method === 'GET') {
+            var params = {
+                FunctionName: url_parsed.query.lambda, /* required */
+                Payload: JSON.stringify({
+                    queryStringParameters: {
+                        email: url_parsed.query.email
+                    },
+                    httpMethod: 'GET'
+                })
+            };
+        } else {
+            var params = {
+                FunctionName: url_parsed.query.lambda, /* required */
+                Payload: JSON.stringify({
+                    body: body,
+                    httpMethod: 'POST'
+                })
+            };
+        }
+        lambda.invoke(params, function(err, data) {
+            if (err) {
+                console.log(err, err.stack);
+                response.setHeader('Content-Type', 'application/json');
+                response.statusCode = 500;
+                response.write(JSON.stringify(err));
+                response.end();
+            } else {
+                console.log(data);
+                response.setHeader('Content-Type', 'application/json');
+                response.statusCode = data.StatusCode;
+                response.write(JSON.parse(data.Payload).body);
+                response.end();
+            }
+        });
     });
 }).listen(3000);
